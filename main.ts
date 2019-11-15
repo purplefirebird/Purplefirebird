9e55e20d6319c9b1389ddb58efce298fb91cf039
purplefirebird <57811880+purplefirebird@users.noreply.github.com>
2019-11-15 14:36:04 -0600

    gyro

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 
 function turnWithGyro() {
     sensors.gyro2.calibrate();
-    motors.largeBC.steer(200, 50, 200, MoveUnit.Degrees);
+    motors.largeBC.steer(200, 50);
     sensors.gyro2.pauseUntilRotated(170);
     motors.stopAll();
 } 
